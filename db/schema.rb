7da12714acbe052d253400827735fabaa6793a46
vita <dragic.hsieh@gmail.com>
2018-05-15 02:32:04 +0800

    Done create comment model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180514171057) do
+ActiveRecord::Schema.define(version: 20180514182956) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"

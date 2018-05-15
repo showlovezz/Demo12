68cb8dc8cf939536e54e9862898e36dd08aa629b
vita <dragic.hsieh@gmail.com>
2018-05-15 11:34:20 +0800

    Modify post controller and add has_many method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-
+	before_action :find_post, only: [:show, :update, :edit, :destroy]
 
 	def index
 		@posts = Post.all.order("created_at DESC")
@@ -20,12 +20,9 @@ class PostsController < ApplicationController
 	end
 
 	def show
-		@post = Post.find(params[:id])
 	end
 
 	def update
-		@post = Post.find(params[:id])
-
 		if @post.update(post_params)
 			redirect_to @post
 		else
@@ -34,11 +31,9 @@ class PostsController < ApplicationController
 	end
 
 	def edit
-		@post = Post.find(params[:id])
 	end
 
 	def destroy
-		@post = Post.find(params[:id])
 		@post.destroy
 
 		redirect_to posts_path
@@ -49,5 +44,8 @@ class PostsController < ApplicationController
 	def post_params
 		params.require(:post).permit(:title, :content)
 	end
-	
+
+	def find_post
+		@post = Post.find(params[:id])
+	end
 end
